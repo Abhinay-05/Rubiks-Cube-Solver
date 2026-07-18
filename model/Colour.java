e97a89bb7d027d339ab760d0edf8887d7426a3d3
Abhinay-05 <abhinaynnd05@gmail.com>
2026-07-19 04:23:31 +0530

    Add files via upload

---
+++ model/Colour.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Colour {
+    WHITE,
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    ORANGE
+}
---
+++ model/Moves.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Moves {
+    U, U_PRIME, U2,
+    F, F_PRIME, F2,
+    L, L_PRIME, L2,
+    R, R_PRIME, R2,
+    B, B_PRIME, B2,
+    D, D_PRIME, D2
+}
